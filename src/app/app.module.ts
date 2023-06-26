6b603b001163313311dde302ce881cbb475b9b6a
M00Alaa <moalaa641@gmail.com>
2023-06-26 21:01:09 +0300

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BooksComponent } from './pages/books/books.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { QuestionsComponent } from './pages/questions/questions.component';
+import { ExamsQuestionsComponent } from './pages/exams-questions/exams-questions.component';
+import { AdminsComponent } from './pages/admins/admins.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    BooksComponent,
+    CategoriesComponent,
+    QuestionsComponent,
+    ExamsQuestionsComponent,
+    AdminsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/exams-questions/exams-questions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exams-questions',
+  templateUrl: './exams-questions.component.html',
+  styleUrls: ['./exams-questions.component.scss']
+})
+export class ExamsQuestionsComponent {
+
+}
