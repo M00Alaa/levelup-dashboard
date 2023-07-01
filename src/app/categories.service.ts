2458ac210ab0737ba97c1e111377920a42d4512f
M00Alaa <moalaa641@gmail.com>
2023-07-01 23:49:49 +0300

    add book component

--- src/app/categories.service.ts
+++ src/app/categories.service.ts
@@ -9,6 +9,7 @@ export class CategoriesService {
 
   constructor(private http: HttpClient) { }
 
+  // categories
   getAllCategories(): Observable<any> {
     return this.http.get(`https://level.somee.com/api/allCategory`)
   }
@@ -23,5 +24,22 @@ export class CategoriesService {
 
   updateCategory(ID: any, data: any): Observable<any> {
     return this.http.put(`https://level.somee.com/api/updatecategory?ID=${ID}`, data)
+  }
+
+  // books
+  getAllBooks(category: any, level: any): Observable<any> {
+    return this.http.get(`https://level.somee.com/api/book?category=${category}&level=${level}`)
+  }
+
+  addBook(data: any): Observable<any> {
+    return this.http.post(`https://level.somee.com/api/Addbook`, data)
+  }
+
+  deleteBook(R_ID: any): Observable<any> {
+    return this.http.delete(`https://level.somee.com/api/delete?R_ID=${R_ID}`)
+  }
+
+  updateBook(R_ID: any, data: any): Observable<any> {
+    return this.http.put(`https://level.somee.com/api/updatebook?R_ID=${R_ID}`, data)
   }
 }
--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
+
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CategoriesService } from 'src/app/categories.service';
 
 @Component({
   selector: 'app-books',
@@ -6,5 +14,141 @@ import { Component } from '@angular/core';
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent {
+
+  categories: any[] = []
+
+  books: any[] = []
+
+  newBookData = {
+    catecoryname: '',
+    levelname: '',
+    book: ''
+  }
+
+  newBookId: any;
+
+  showAdd!: boolean;
+  showUpdate!: boolean;
+
+
+
+  bookData: FormGroup = new FormGroup({
+    catecoryname: new FormControl(null, [Validators.required]),
+    levelname: new FormControl(null, [Validators.required]),
+    book: new FormControl(null, [Validators.required])
+  })
+
+
+  bookCategoryLevel: FormGroup = new FormGroup({
+    category: new FormControl(null),
+    level: new FormControl(null)
+  })
+
+  // success
+  addBook() {
+
+    console.log(this.bookData.value);
+
+    this._CategoriesService.addBook(this.bookData.value).subscribe({
+      next: (res) => {
+        console.log(res);
+
+        this.bookData.reset()
+        this.getAllBooks()
+      }
+    })
+  }
+
+  // success
+  getAllBooks() {
+
+    console.log(this.bookCategoryLevel.value);
+
+    this._CategoriesService.getAllBooks(this.bookCategoryLevel.value.category, this.bookCategoryLevel.value.level).subscribe({
+      next: (res) => {
+        this.books = res.data;
+
+        console.log(this.books);
+
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
+  deleteBook(ID: number) {
+    this._CategoriesService.deleteBook(ID).subscribe({
+      next: (res) => {
+        alert('deleted successfully')
+
+        this.getAllBooks();
+      },
+      error: (err) => {
+        console.log(err);
+
+      }
+    })
+  }
+
+  getBookInfo(data: any) {
+    this.showAdd = false;
+    this.showUpdate = true;
+
+    this.bookData.controls['catecoryname'].setValue(this.bookCategoryLevel.value.category)
+    this.bookData.controls['levelname'].setValue(this.bookCategoryLevel.value.level)
+    this.bookData.controls['book'].setValue(data.book)
+
+    this.newBookId = data.R_ID
+  }
+
+
+  updateCategory() {
+
+    this.newBookData.catecoryname = this.bookData.value.catecoryname
+    this.newBookData.levelname = this.bookData.value.levelname
+    this.newBookData.book = this.bookData.value.book
+
+
+    this._CategoriesService.updateBook(this.newBookId, this.newBookData).subscribe({
+      next: (res) => {
+        console.log(res);
+
+        alert('updated successfully')
+        this.bookData.reset()
+        this.getAllBooks()
+      },
+      error: (err) => {
+        console.log(err);
+
+      }
+    })
+  }
+
+
+  clickAddBook() {
+    this.bookData.reset();
+    this.showAdd = true;
+    this.showUpdate = false;
+  }
+
+  constructor(
+    private _AuthService: AuthService, private _Router: Router, private _CategoriesService: CategoriesService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllCategories()
+  }
+
+  getAllCategories() {
+    this._CategoriesService.getAllCategories().subscribe({
+      next: (res) => {
+        this.categories = res.data;
+      },
+      error: (err) => {
+        console.log('getAllCategories' + err);
+      }
+    })
+  }
 
 }
